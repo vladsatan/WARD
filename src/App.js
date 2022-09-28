91bd1ee34c5f65df01aac8c5aabb1a8d264b29de
vladsatan <vladus007445@gmail.com>
2022-09-29 00:14:28 +0300

    create main section and header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.scss';
+import Header from './Components/Header/Header';
+import MainSection from './Components/MainSection/MainSection';
+
 
 function App() {
   return (
     <div className="App">
-      
+       <Header />
+       <MainSection />
     </div>
   );
 }
