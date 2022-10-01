0e96023cb6a7b082b40fdd8569b0a9b0a78648f1
vladsatan <vladus007445@gmail.com>
2022-10-01 18:38:41 +0300

    change margin in Strategy

--- src/Components/Strategy/Strategy.jsx
+++ src/Components/Strategy/Strategy.jsx
@@ -1,74 +1,68 @@
 import "../Strategy/Strategy.scss";
+import arroy1 from "../../Logo/arroy.svg"
+import arroy2 from "../../Logo/arroy2.svg"
+import arroy3 from "../../Logo/arroy3.svg"
 
 const Strategy = () => {
-  const dataArray = [
-    {
-      header: "Business Essential ",
-      text1: "Designed for businesses with basic IT requirements ",
-      header2: "What’s included?",
-      text2: [
-        "24/7 Systems Monitoring ",
-        "Security Management",
-        "Patch Management",
-        "Computer Backups",
-        "Email Support",
-      ],
-      button: "Get your Essential now",
-      way: 1,
-    },
-    {
-      header: "Business Essential ",
-      text1: "Designed for businesses with basic IT requirements ",
-      header2: "What’s included?",
-      text2: [
-        "24/7 Systems Monitoring ",
-        "Security Management",
-        "Patch Management",
-        "Computer Backups",
-        "Email Support",
-      ],
-      button: "Get your Essential now",
-      way: 2,
-    },
-    {
-      header: "Business Essential ",
-      text1: "Designed for businesses with basic IT requirements ",
-      header2: "What’s included?",
-      text2: [
-        "24/7 Systems Monitoring ",
-        "Security Management",
-        "Patch Management",
-        "Computer Backups",
-        "Email Support",
-      ],
-      button: "Get your Essential now",
-      way: 3,
-    },
-  ];
+  
   return (
     <div className="Startegy-container">
       <h2 className="Strategy_header">
         Flexible <span className="Strategy-red">Strategy</span> For Everyone
       </h2>
       <div className="Strategy-items-container">
-        {dataArray.map((el) => (
+
           <div className="Strategy-item">
             <div className="Strategy-block">
-              <h5 className="Startegy-head1">{el.header}</h5>
-              <p className="Startegy-text1">{el.text1}</p>
-              <h5 className="Startegy-head2">{el.header2}</h5>
-              <div className="Startegy-way1">
-                <div className="Strategy-left"></div>
-              </div>
+              <h5 className="Startegy-head1">Business Essential</h5>
+              <p className="Startegy-text1">Designed for businesses with basic IT requirements</p>
+              <h5 className="Startegy-head2">What’s included?</h5>
+              <img className="arroy" src={arroy1} alt='arroy' />
               <ul className="Startegy-text2">
-                {el.text2.map((el) => (
-                  <li>{el}</li>
-                ))}
+               <li>24/7 Systems Monitoring</li>
+               <li>Security Management</li>
+               <li>Patch Management</li>
+               <li>Computer Backups</li>
+               <li>Email Support</li>
               </ul>
             </div>
-            <button className="Strategy-button">{el.button}</button>
+            <button className="Strategy-button">Get your Essential now</button>
           </div>
-        ))}
+
+          <div className="Strategy-item">
+            <div className="Strategy-block">
+              <h5 className="Startegy-head1">Business Premium</h5>
+              <p className="Startegy-text12">Designed for businesses looking to eliminate costly break/fix IT services.</p>
+              <h5 className="Startegy-head2">All Business Essential, plus:</h5>
+              <img className="arroy" src={arroy2} alt='arroy' />
+              <ul className="Startegy-text2">
+               <li>Preventive Maintenance</li>
+               <li>Asset Management</li>
+               <li>Secure Cloud Backup</li>
+               <li>Workstation Support</li>
+               <li>Server/Network Support</li>
+               <li>Onsite/Phone Support</li>
+              </ul>
+            </div>
+            <button className="Strategy-button">Get your Premium now</button>
+          </div>
+
+          <div className="Strategy-item">
+            <div className="Strategy-block">
+              <h5 className="Startegy-head1">Business Pro</h5>
+              <p className="Startegy-text13">A fully comprehensive plan for any business size or needs.</p>
+              <h5 className="Startegy-head2">All Business Essential and Premium, plus:</h5>
+              <img className="arroy" src={arroy3} alt='arroy' />
+              <ul className="Startegy-text2">
+               <li>Reporting</li>
+               <li>Vendor Management</li>
+               <li>Virtual CIO (vCIO)</li>
+               <li>Desktop Equipment Lease</li>
+               <li>Network Equipment Lease</li>
+             </ul>
+            </div>
+            <button className="Strategy-button">Get your Pro now</button>
+          </div>
       </div>
     </div>
   );
